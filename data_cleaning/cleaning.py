cbbaddac3b74e78766c9718dd049d859e3ac6a62
RileyTan <149601796+RileyTan@users.noreply.github.com>
2025-01-07 16:33:23 +0800

    Update cleaning.py
    docstrings edited for clarity

--- data_cleaning/cleaning.py
+++ data_cleaning/cleaning.py
@@ -13,9 +13,8 @@ def main():
 
 def select_faulty(df):
     """
-    This function returns a mask, used in Pandas, a Series of booleans.
-    Rows that have an empty or negative ranking will be represented by True.
-    Rows that don't have that error will be represented by False.
+    looking at original_country_by_hdi.csv, we isolate the rows with issues by looking at the Rank column
+    all rows with issue either have an empty rank or a rank containing non-numeric characters
     """
     mask_empty_rank = df['Rank'].isnull()
     mask_negative_rank = df['Rank'].str.contains('[^0-9.]')
@@ -26,37 +25,41 @@ def select_faulty(df):
     return df
 def fix_round1(df, faulty_rows):
     """
-    This function shifts the faulty rows one column to the right.
-    This function then returns the corrected dataframe, corrected once.
+    this function shifts the faulty rows one column to the right.
     """
+    
     df.loc[faulty_rows] = df.loc[faulty_rows].shift(periods=1, axis="columns")
 
 def clean_percentage_values(df, columns):
     """
-    This function removes percentage signs and converts the values to floats.
+    this function removes percentage signs and converts the values to floats.
     """
+    
     for column in columns:
         df[column] = df[column].str.rstrip('%').astype(float) / 100.0
 
 def fix_round2(df, faulty_rows):
     """
-    This function returns the corrected dataframe by shifting the rows that have cells with % inside.
-    This function works with the same rows as before.
+    this is the final shift of columns for annual growth values into its corresponding column
+    all percentage values are removed and converted to floats for easier future use 
     """
+    
     last_two_columns = df.columns[-2:]
     clean_percentage_values(df, last_two_columns)
     df.loc[faulty_rows, last_two_columns] = df.loc[faulty_rows, last_two_columns].shift(periods=1, axis="columns")
 
 def fix_round3(df, faulty_rows):
     """
-    Rewrites the Rank column sequentially.
+    rewrites the Rank column sequentially.
     """
+    
     df["Rank"] = range(1, len(df) + 1)
 
 def save_to_csv(df, file_path='correct_countries_by_hdi.csv'):
     """
-    Saves the corrected dataframe into a new file.
+    saves the corrected dataframe into a new csv file.
     """
+    
     df.to_csv(file_path, index=False)
 
 if __name__ == "__main__":
